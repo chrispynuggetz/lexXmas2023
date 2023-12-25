08bc92f86d20c56504238f283342cb992ce5ea0e
chrispynuggetz <133781296+chrispynuggetz@users.noreply.github.com>
2023-12-25 08:52:19 -0600

    Update script.js

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ function handleClick(){
                     const element = giftBox[0];
   let currentNumber = 0;
 
-  const interval = 10000 / 100;
+  const interval = 20000 / 120;
   const intervalId = setInterval(() => {
     currentNumber++;
     element.textContent = '$'+currentNumber;
           giftBox[0].style.display = "block";
         console.log('hi',currentNumber);
-    if (currentNumber >= 100) {
+    if (currentNumber >= 120) {
       clearInterval(intervalId);
     }
   }, interval);
